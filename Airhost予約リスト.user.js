67e56563a66cafc246a95b573015fda70120ea74
Andrew Thoreson <48644513+Altigraph@users.noreply.github.com>
2019-06-05 16:26:49 +0900

    const/let; forEach

--- Airhost予約リスト.user.js
+++ Airhost予約リスト.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Airhost予約リスト
 // @namespace    https://www.faminect.jp/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Airhost予約画面から→すぐにSHARE!
 // @author       草村安隆 Andrew Lucian Thoreson
 // @downloadURL  https://github.com/Altigraph/QMTM/raw/master/Airhost%E4%BA%88%E7%B4%84%E3%83%AA%E3%82%B9%E3%83%88.user.js
@@ -12,20 +12,20 @@
 // ==/UserScript==
 
 (function () {
-    var months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"];
-    var rows = document.getElementsByClassName("parent");
-    var output= "";
-    for (var i = 0; i < rows.length; i++) {
-        var link = "https://cloud.airhost.co/en/bookings/" + rows[i].childNodes[0].innerHTML.match(/\d{7}/);
-        var guestName = rows[i].childNodes[0].innerText;
-        var CI = rows[i].childNodes[1].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$3." +
-          (months.indexOf(rows[i].childNodes[1].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$1")) + 1) + ".$2");
-        var CO = rows[i].childNodes[2].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$3." +
-          (months.indexOf(rows[i].childNodes[2].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$1")) + 1) + ".$2");
-        var property = rows[i].childNodes[3].innerText;
+    const months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"];
+    const rows = document.querySelectorAll(".parent");
+    let output;
+    rows.forEach(row => {
+        let link = "https://cloud.airhost.co/en/bookings/" + row.childNodes[0].innerHTML.match(/\d{7}/);
+        let guestName = row.childNodes[0].innerText;
+        let CI = row.childNodes[1].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$3." +
+          (months.indexOf(row.childNodes[1].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$1")) + 1) + ".$2");
+        let CO = row.childNodes[2].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$3." +
+          (months.indexOf(row.childNodes[2].innerText.replace(/(.*?)\s(\d*)\s(\d{4})/, "$1")) + 1) + ".$2");
+        let property = row.childNodes[3].innerText;
 
         output += property + "\n" + guestName + "\n" + CI + "-" + CO + "\n" + link + "\n\n";
-    }
+    });
 
-    GM_setClipboard(output);
+    output ? GM_setClipboard(output) : window.alert("予約なし")
 })();

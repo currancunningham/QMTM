8a91a44ad2d2ab35d32aae20f25d64b736fc1362
Andrew Thoreson <48644513+Altigraph@users.noreply.github.com>
2019-06-05 16:30:20 +0900

    refactoring

--- Bookingログイン.user.js
+++ Bookingログイン.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bookingログイン
 // @namespace    https://www.faminect.jp/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Booking Loginをタッチレスに
 // @author       草村安隆 Andrew Lucian Thoreson
 // @downloadURL  https://github.com/Altigraph/QMTM/raw/master/Booking%E3%83%AD%E3%82%B0%E3%82%A4%E3%83%B3.user.js
@@ -10,10 +10,11 @@
 // @run-at       document-idle
 // ==/UserScript==
 setInterval(() => {
-    if (document.getElementsByClassName("bui-form__error").length !== 0 &&
-        document.getElementsByClassName("bui-form__error")[0].innerText !== "Enter your username" &&
-        document.getElementsByClassName("bui-form__error")[0].innerText !== "ユーザー名を入力してください") {
-        return;
-    }
-    document.getElementsByClassName("bui-button bui-button--primary bui-button--large bui-button--wide")[0].click();
+  const msg = document.querySelector(".bui-form__error");
+  if (msg &&
+      msg.innerText !== "Enter your username" &&
+      msg.innerText !== "ユーザー名を入力してください") {
+      return;
+  }
+  document.getElementsByClassName("bui-button bui-button--primary bui-button--large bui-button--wide")[0].click();
 }, 200);

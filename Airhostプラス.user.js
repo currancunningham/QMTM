39a7f99bfaad3202c3164de80acb2395496f70b5
Andrew Thoreson <48644513+Altigraph@users.noreply.github.com>
2019-06-11 15:16:13 +0900

    Adding update links

--- Airhostプラス.user.js
+++ Airhostプラス.user.js
@@ -4,8 +4,8 @@
 // @version      0.1
 // @description  Airbnb画面をプラスα
 // @author       草村安隆 Andrew Lucian Thoreson
-// @downloadURL
-// @updateURL
+// @downloadURL  https://github.com/Altigraph/QMTM/raw/master/Airhost%E3%83%97%E3%83%A9%E3%82%B9.user.js
+// @updateURL    https://github.com/Altigraph/QMTM/raw/master/Airhost%E3%83%97%E3%83%A9%E3%82%B9.user.js
 // @include      https://cloud.airhost.co/*
 // @run-at       document-idle
 // ==/UserScript==

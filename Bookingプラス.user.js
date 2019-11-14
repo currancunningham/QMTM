abf41c7ecbab1db4be4684a6eb0a18218d9628c9
Andrew Thoreson <48644513+sinaraheneba@users.noreply.github.com>
2019-11-14 12:06:33 +0900

    Update Bookingプラス.user.js

--- Bookingプラス.user.js
+++ Bookingプラス.user.js
@@ -4,8 +4,8 @@
 // @version      0.1
 // @description  Booking画面をプラスα
 // @author       草村安隆 Andrew Lucian Thoreson
-// @downloadURL  https://github.com/sinaraheneba/QMTM/raw/master/Booking%E3%83%97%E3%83%A9%E3%82%B9.user.js
-// @updateURL    https://github.com/sinaraheneba/QMTM/raw/master/Booking%E3%83%97%E3%83%A9%E3%82%B9.user.js
+// @downloadURL  https://github.com/currancunningham/QMTM/raw/master/Booking%E3%83%97%E3%83%A9%E3%82%B9.user.js
+// @updateURL    https://github.com/currancunningham/QMTM/raw/master/Booking%E3%83%97%E3%83%A9%E3%82%B9.user.js
 // @include      https://admin.booking.com/*
 // @resource     settings file:///C:/Program Files/QMTM/settings.json
 // @resource     mac_settings file:///Users/Shared/settings.json

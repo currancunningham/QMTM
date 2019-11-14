a723b28daa81577141b158667e28fce183423e59
currancunningham <56859128+currancunningham@users.noreply.github.com>
2019-11-14 14:36:31 +0900

    Updated Version and URL

--- Airbnbリスティングリンク.user.js
+++ Airbnbリスティングリンク.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Airbnbリスティング飛ぶ
 // @namespace    https://www.faminect.jp/
-// @version      0.1
+// @version      0.1.1
 // @description  ANYサイトから、Airbnbまで繋がっていこう
 // @author       草村安隆 Andrew Lucian Thoreson
-// @downloadURL  https://github.com/sinaraheneba/QMTM/raw/master/Airbnb%E3%83%AA%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E3%83%AA%E3%83%B3%E3%82%AF.user.js
-// @updateURL    https://github.com/sinaraheneba/QMTM/raw/master/Airbnb%E3%83%AA%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E3%83%AA%E3%83%B3%E3%82%AF.user.js
+// @downloadURL  https://github.com/currancunningham/QMTM/raw/master/Airbnb%E3%83%AA%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E3%83%AA%E3%83%B3%E3%82%AF.user.js
+// @updateURL    https://github.com/currancunningham/QMTM/raw/master/Airbnb%E3%83%AA%E3%82%B9%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E3%83%AA%E3%83%B3%E3%82%AF.user.js
 // @run-at       context-menu
 // ==/UserScript==
 
